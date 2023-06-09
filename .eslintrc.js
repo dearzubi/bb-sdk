ff1dafc8c6acda298579b97af7d7e92c358e9f7e
Zubair Khalid <zubairkhalidce@gmail.com>
2023-06-09 11:56:53 +0100

    configured the monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  ignorePatterns: ['dist'],
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended' // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
